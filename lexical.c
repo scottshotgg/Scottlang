a6c338332af57040af5b1a614fca6942f058f6bd
scottshotgg <kind3rgartenlunchbox@gmail.com>
2016-03-06 23:17:46 -0600

    added a todo.readme to keep track of what we are doing and need to do, added more operators - numbers will change later to be more sequential, added new types

--- lexical.c
+++ lexical.c
@@ -12,25 +12,56 @@ int hasDecimal = 0;
 FILE *file_reader, *fopen();
 
 
+/*	
+	Make a switch or use an associative array to pack this into a coherent 
+	data structure that will allow us to unpack the names from it when we 
+	print out and start to use the debug flag
+*/
 
-#define ALPHA		0
-#define DIGIT		1
-#define DECIMAL		2
-#define OTHER		99
-
-#define INT			10
-#define VAR			11
-#define FLOAT		12
-#define ASSIGN_OP	20
-#define ADD_OP		21
-#define SUB_OP		22
-#define MULT_OP		23
-#define DIV_OP		24
-#define LEFT_PAREN	25
-#define	RIGHT_PAREN	26
-#define END_OP		27 
+/* Char types */
+#define ALPHA				0
+#define DIGIT				1
+#define DECIMAL				2
+#define OTHER				99
 
 
+/* Types */
+#define INT					10
+#define VAR					11
+#define FLOAT				12
+#define CHAR				13
+#define STRING				14
+
+
+/* Operators */
+#define ASSIGN_OP			20
+#define ADD_OP				21
+#define SUB_OP				22
+#define MULT_OP				23
+#define DIV_OP				24
+#define ADD_ASSIGN_OP		28
+#define SUB_ASSIGN_OP		29
+#define MULT_ASSIGN_OP		30
+#define DIV_ASSIGN_OP		31
+#define DOT_ADD_OP			32
+#define DOT_MULT_OP			33
+#define DOT_SUB_OP			34
+#define DOT_DIV_OP			35
+
+
+/* Grouping symbols */
+#define LEFT_PAREN			25
+#define	RIGHT_PAREN			26
+
+
+/* End statement */
+#define END_STATEMENT		27 
+
+
+
+int dummyFunc() {
+	return ('=' == 1);		// experimenting with this, will throw a warning for now
+}
 
 
 void getChar() {
@@ -97,7 +128,7 @@ int lookupTable(char unkownChar) {
 
 		case ';':
 			addChar();
-			nextToken = END_OP;
+			nextToken = END_STATEMENT;
 			break;
 
 		case '=':
@@ -155,7 +186,7 @@ int lex() {
 			hasDecimal = 0;
 			break;
 
-		case DECIMAL:
+		/*case DECIMAL:		// Don't think we need this, but I'll leave it anyways for now
 			addChar();
 			getChar();
 			while(nextCharType == DIGIT) {
@@ -164,7 +195,7 @@ int lex() {
 			}
 
 			nextToken = FLOAT;
-			break;
+			break;*/
 
 		case OTHER:
 			lookupTable(nextChar);
@@ -190,6 +221,12 @@ int lex() {
 }
 
 int main(int argc, char *argv[]) {
+
+	//int me = dummyFunc;
+
+	//printf("%d\n\n\n\n", me);
+
+
 	if(argc > 2) {
 		if((file_reader = fopen(argv[1], "r")) == NULL) {
 			printf("Program \"%s\" does not exist\n", argv[1]);
@@ -203,9 +240,13 @@ int main(int argc, char *argv[]) {
 				int error = lex();
 				if(error == -2) {
 					printf("Syntax error: More than one decimal in float constant\n");
-					return -2;
+					return -2;		// this should change, figure out a way to continue parsing 
+									// and then collect all errors at the end and display those 
+									// at the end
 				}
 			} while(nextChar != EOF);
+
+			printf("\nParsing completed: no errors\n");
 		}
 	}
 	else {
