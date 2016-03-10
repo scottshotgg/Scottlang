f776ff04be1e357e9acd61a4ca76b581b7c05f91
Scott Gaydos <kind3rgartenlunchbox@gmail.com>
2016-03-10 00:33:01 -0600

    added some backend code - golang can now run the ScottLang lexer, added an ajax request to index, and removed overflow

--- website/BackEnd/main.go
+++ website/BackEnd/main.go
@@ -27,8 +27,28 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func exe_cmd(cmd string, wg *sync.WaitGroup) {
+  fmt.Println("command is ",cmd)
+  // splitting head => g++ parts => rest of the command
+  parts := strings.Fields(cmd)
+  head := parts[0]
+  parts = parts[1:len(parts)]
+
+  out, err := exec.Command(head,parts...).Output()
+  if err != nil {
+    fmt.Printf("%s", err)
+  }
+  fmt.Printf("%s", out)
+  wg.Done() // Need to signal to waitgroup that this goroutine is done
+}
+
+
 func main() {
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources")))) 
 	http.HandleFunc("/", hello)
+
+	exe_cmd("ls -la")
+	log.Println("ls")
 	http.ListenAndServe(":8080", nil)
+
 }
--- website/main.go
+++ website/main.go
@@ -6,11 +6,19 @@ import (
 	"net/http"
 	"html/template"
 	"encoding/json"
+	"sync"
+	"strings"
+	"os/exec"
 )
 
 type genericResponse struct {
 	Success bool   `json:"success"`
-	Err     string `json:"err"`
+}
+
+type GenericHandler struct {
+	GET  func(http.ResponseWriter, *http.Request)
+	PUT  func(http.ResponseWriter, *http.Request)
+	POST func(http.ResponseWriter, *http.Request)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -27,8 +35,82 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func exe_cmd(cmd string, wg *sync.WaitGroup) {
+	fmt.Println("command is ",cmd)
+	// splitting head => g++ parts => rest of the command
+	parts := strings.Fields(cmd)
+	head := parts[0]
+	parts = parts[1:len(parts)]
+
+	out, err := exec.Command(head,parts...).Output()
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Printf("%s", out)
+	wg.Done() // Need to signal to waitgroup that this goroutine is done
+}
+
+func app(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("in the function and shit")
+	r.ParseForm()
+	log.Println(r.Form)
+	response, _ := json.Marshal(genericResponse{Success: true})
+	w.Write(response)
+
+}
+
+func (this GenericHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	//sessionCookie, _ := req.Cookie("session")
+	//host, _, _ := net.SplitHostPort(req.RemoteAddr)
+	/*
+		If we're at the same IP address as the server, the host should be ::1 (ipv6 localhost)
+	*/
+	//switch {
+	//case host == "::1":		// this currently only works on chrome for some reason
+		/*
+			The request is coming from the server. Let it through by default.
+		*/
+	//case sessionCookie == nil || !database.IsSessionHashValid(sessionCookie.Value):
+	//	redirectToLoginHandler(w, req)
+	//	log.Println("Redirected to login")
+	//	return
+	//}
+
+	/*
+		Generic stuff goes here
+	*/
+		
+	if req.Method == "GET" && this.GET != nil {
+		this.GET(w, req)
+	} else if req.Method == "POST" && this.POST != nil {
+		this.POST(w, req)
+	} else if req.Method == "PUT" && this.PUT != nil {
+		this.PUT(w, req)
+	} else {
+		http.Error(w, "GenericHandler error", http.StatusInternalServerError)
+		log.Fatalf("No handler specified for the request %s", req)
+	}
+}
+
 func main() {
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources")))) 
 	http.HandleFunc("/", hello)
+	//http.Handle("/app", GenericHandler{GET: app})
+	http.HandleFunc("/app", app)
+
+	var wg sync.WaitGroup
+    wg.Add(1)
+   /* wg.Add(1)
+    go dosomething(400, &wg)
+    wg.Add(1)
+    go dosomething(150, &wg)
+    wg.Add(1)
+    go dosomething(600, &wg)
+
+    wg.Wait()
+    fmt.Println("Done")*/
+
+	exe_cmd("./../lexical ../program 0", &wg)
 	http.ListenAndServe(":8080", nil)
+
 }
